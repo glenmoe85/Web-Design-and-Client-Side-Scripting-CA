e72e8309bf3c06870d8857bf7ea72a12c5dea732
ford99 <92645091+ford99@users.noreply.github.com>
2021-12-08 15:19:41 +0000

    Delete Project_Waterford - Copy2 directory

--- Project_Waterford - Copy2/Project_Waterford - Copy/js/script.js
+++
@@ -1,8 +0,0 @@
-function resNavBar() {
-  var x = document.getElementById("Navbar");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
