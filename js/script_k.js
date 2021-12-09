09ce4474427fa651590e4833b9f47124b291d30e
ForrestGumpingIT <92694825+ForrestGumpingIT@users.noreply.github.com>
2021-12-09 12:15:29 +0000

    js modal update

--- js/script_k.js
+++ js/script_k.js
@@ -1,48 +1,6 @@
-//declare var for the collapsables
-var coll = document.getElementsByClassName("collapsible");
-var i;
 
-//collapsable action
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", 
-  function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-} 
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
-
-// Get the modal
+	// Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
@@ -70,58 +28,58 @@ window.onclick = function(event) {
 
 
 
-// Modal two
-// Get the modal
-var modal1 = document.getElementById("myModal1");
+	// Get the modal
+var modal1 = document.getElementById("modal1");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn1");
+var btn = document.getElementById("btn1");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal1.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal1.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal1.style.display = "none";
   }
 } 
 
-// Modal three
-// Get the modal
-var modal = document.getElementById("myModal2");
+	// Get the modal
+var modal2 = document.getElementById("modal2");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn2");
+var btn = document.getElementById("btn2");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[2];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal2.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal2.style.display = "none";
   }
 } 
 
-
+jQuery(document).ready(function(){
+    $.getScript('collscript.js');
+});
