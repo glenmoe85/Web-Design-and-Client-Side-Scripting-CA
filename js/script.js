d79b9edf639bce4571533e913cb7f15be594686c
GlenMoe85 <glenmoe85@gmail.com>
2021-12-09 22:05:02 +0000

    adding alert box for contact form

--- js/script.js
+++ js/script.js
@@ -8,5 +8,10 @@ function resNavBar() {
 }
 
 function formSubmit() {
-	document.forms['form_name'].reset();
+	var fn = document.contactForm.firstname.value;
+	var ln = document.contactForm.lastname.value;
+	var em = document.contactForm.email.value;
+	alert("Thank you for completing the form " +fn+" "+ln+"\nA member of our team will be respond shortly to "+em);
+	document.forms['contactForm'].reset();
+	
 }
